5277038b93de3c12e5551e19b63463f53b2d97c6
Yuriy Meresin <yuriymeresin@gmail.com>
2025-10-11 21:21:48 +0300

    clear

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
 'use strict';
-
-const flinghts = ['Russia', 'USA', 'Cezh', 'Cezh', 'USA'];
-const setFlights = new Set(flinghts);
-//console.log(setFlights);
-//console.log(setFlights.size)
-//console.log(setFlights.has('Russia'))
-//setFlights.add('Paris');
-//setFlights.delete('Russia');
-//console.log(setFlights);
-
-for (const flight of setFlights) {
-	console.log(flight)
-}
-
-console.log([...setFlights])

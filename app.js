adaaeb21f801c9d0c005fdfe4bfd84d6415c413a
Yuriy Meresin <yuriymeresin@gmail.com>
2025-10-21 22:25:44 +0300

    dice game

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const dice = {
+  d4: 'd4',
+  d6: 'd6',
+  d8: 'd8',
+  d10: 'd10',
+  d12: 'd12',
+  d16: 'd16',
+  d20: 'd20'
+};
+
+function rollDice(diceNotation) {
+  const sides = parseInt(String(diceNotation).slice(1));
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+console.log(rollDice(dice.d20));

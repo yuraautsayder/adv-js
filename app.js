69652b87db350d9e6cae94d1c160fb5479cbfd1e
Yuriy Meresin <yuriymeresin@gmail.com>
2025-11-09 14:45:11 +0300

    age validate

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+// Сделать функцию валидации возраста. На вход передаётся строка даты рождения пользователя вида “2022-01-01”.
+
+// Необходимо вывести true, если ему больше 14 лет и false, если меньше
+//
+//
+
+function validateDate(dateString) {
+	const currentDate = new Date();
+	const brithDate = new Date(dateString);
+	const age = currentDate - brithDate;
+	return age > 14 * 365 * 24 * 60 * 60 * 1000;
+}

cbbdc772225d7b5b35a4087ee551044c4d397eae
Yuriy Meresin <yuriymeresin@gmail.com>
2025-12-18 23:54:26 +0300

    sv

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-"use strict";
+'use strict';
+
+function pizzaTimer(time) {
+	const end = new Date().getTime() + time;
+	const interval = setInterval(() => {
+		console.log(
+			new Intl.DateTimeFormat('ru-RU', {
+				minute: 'numeric',
+				second: 'numeric',
+			}).format(new Date(end + 100 - new Date())),
+		);
+	}, 1000);
+	setTimeout(() => {
+		clearInterval(interval);
+		console.log('pizza ready!!!');
+	}, time);
+}
+
+pizzaTimer(5000);

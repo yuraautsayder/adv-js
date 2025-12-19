eaae99cd1a4a9355cfb3a057b8e696f7516bd637
Yuriy Meresin <yuriymeresin@gmail.com>
2025-12-19 23:31:35 +0300

    cart

--- app.js
+++ app.js
@@ -1,19 +1,72 @@
 'use strict';
+// const Book = function(title, author) {
+// 	this.title = title;
+// 	this.author = author;
+// 	this.isRead = false;
+// }
 
-function pizzaTimer(time) {
-	const end = new Date().getTime() + time;
-	const interval = setInterval(() => {
-		console.log(
-			new Intl.DateTimeFormat('ru-RU', {
-				minute: 'numeric',
-				second: 'numeric',
-			}).format(new Date(end + 100 - new Date())),
-		);
-	}, 1000);
-	setTimeout(() => {
-		clearInterval(interval);
-		console.log('pizza ready!!!');
-	}, time);
-}
+// Book.prototype.read = function() {
+// 	this.isRead = true;
+// }
 
-pizzaTimer(5000);
+// const bookNameOne = new Book('Book name one', 'writer');
+// bookNameOne.read()
+// // console.log(bookNameOne);
+// console.log(bookNameOne.__proto__);
+
+// Цель:
+
+//     Написать функцию конструктор и методы для работы с корзиной товаров.
+
+// Основные пункты и задачи:
+
+//     Определение функции конструктора Cart:
+//         Создание корзины (Cart) без принимаемых аргументов.
+//         Создание пустого массива товаров (products) в качестве свойства корзины.
+//     Добавление метода AddProduct:
+//         Функция принимает продукт и добавляет его в массив products, если такого продукта нет (проверка по идентификатору).
+//     Реализация метода увеличения количества товара (increaseAmount):
+//         Функция принимает идентификатор продукта и увеличивает его количество на единицу.
+//     Реализация метода уменьшения количества товара (decreaseAmount):
+//         Функция принимает идентификатор продукта и уменьшает его количество на единицу.
+//         Продукты с количеством 0 удаляются из корзины.
+const product = { id: 1, name: 'Bread', count: 1 };
+
+const Cart = function () {
+	this.products = [];
+};
+
+Cart.prototype.addProduct = function (product) {
+	if (this.products.find((product) => product.id === product.id)) {
+		return;
+	}
+	this.products.push(product);
+};
+
+Cart.prototype.increaseAmount = function (id) {
+	this.products = this.products.map((product) => {
+		if (product.id == id) {
+			product.count++;
+			return product;
+		}
+		return product;
+	});
+};
+Cart.prototype.decreaseAmount = function (id) {
+	this.products = this.products
+		.map((product) => {
+			if (product.id == id) {
+				product.count--;
+				return product;
+			}
+			return product;
+		})
+		.filter(product => product.count > 0);
+};
+
+const cart = new Cart();
+cart.addProduct(product);
+cart.increaseAmount(1);
+cart.decreaseAmount(1);
+cart.decreaseAmount(1);
+console.log(cart)

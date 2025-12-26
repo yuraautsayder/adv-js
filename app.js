99822a1e468f09c9300b6105b44de2f1d27555ed
Yuriy Meresin <yuriymeresin@gmail.com>
2025-12-27 00:51:47 +0300

    todo

--- app.js
+++ app.js
@@ -30,43 +30,48 @@
 //     Реализация метода уменьшения количества товара (decreaseAmount):
 //         Функция принимает идентификатор продукта и уменьшает его количество на единицу.
 //         Продукты с количеством 0 удаляются из корзины.
-const product = { id: 1, name: 'Bread', count: 1 };
+// const product = { id: 1, name: 'Bread', count: 1 };
 
-const Cart = function () {
-	this.products = [];
-};
+// const Cart = function () {
+// 	this.products = [];
+// };
 
-Cart.prototype.addProduct = function (product) {
-	if (this.products.find((product) => product.id === product.id)) {
-		return;
-	}
-	this.products.push(product);
-};
+// Cart.prototype.addProduct = function (product) {
+// 	if (this.products.find((product) => product.id === product.id)) {
+// 		return;
+// 	}
+// 	this.products.push(product);
+// };
 
-Cart.prototype.increaseAmount = function (id) {
-	this.products = this.products.map((product) => {
-		if (product.id == id) {
-			product.count++;
-			return product;
-		}
-		return product;
-	});
-};
-Cart.prototype.decreaseAmount = function (id) {
-	this.products = this.products
-		.map((product) => {
-			if (product.id == id) {
-				product.count--;
-				return product;
-			}
-			return product;
-		})
-		.filter(product => product.count > 0);
-};
+// Cart.prototype.increaseAmount = function (id) {
+// 	this.products = this.products.map((product) => {
+// 		if (product.id == id) {
+// 			product.count++;
+// 			return product;
+// 		}
+// 		return product;
+// 	});
+// };
+// Cart.prototype.decreaseAmount = function (id) {
+// 	this.products = this.products
+// 		.map((product) => {
+// 			if (product.id == id) {
+// 				product.count--;
+// 				return product;
+// 			}
+// 			return product;
+// 		})
+// 		.filter(product => product.count > 0);
+// };
 
-const cart = new Cart();
-cart.addProduct(product);
-cart.increaseAmount(1);
-cart.decreaseAmount(1);
-cart.decreaseAmount(1);
-console.log(cart)
+// const cart = new Cart();
+// cart.addProduct(product);
+// cart.increaseAmount(1);
+// cart.decreaseAmount(1);
+// cart.decreaseAmount(1);
+// console.log(cart)
+
+
+
+// maybe help
+// Object.create(Person.prototype)

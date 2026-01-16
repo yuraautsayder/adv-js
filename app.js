9dd939324bfdb02bed57a3cfadcd81e1f4bab3f9
Yuriy Meresin <yuriymeresin@gmail.com>
2026-01-16 21:37:22 +0300

    nasledovanie do es6

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
 'user sctrict';
 
-class Film {
-	#name;
-	#author;
-	rating;
-	#length;
+// class Film {
+// 	#name;
+// 	#author;
+// 	rating;
+// 	#length;
 
-	constructor(name, author, length) {
-		this.#name = name;
-		this.#author = author;
-		this.#length = length;
-	}
+// 	constructor(name, author, length) {
+// 		this.#name = name;
+// 		this.#author = author;
+// 		this.#length = length;
+// 	}
 
-	get name() {
-		return this.#name;
-	}
+// 	get name() {
+// 		return this.#name;
+// 	}
 
-	get author() {
-		return this.#author;
-	}
+// 	get author() {
+// 		return this.#author;
+// 	}
 
-	get length() {
-		return this.#length;
-	}
-}
+// 	get length() {
+// 		return this.#length;
+// 	}
+// }
 
-const film = new Film('Avatar', 'Cameron', 240);
-console.log(film);
+// const film = new Film('Avatar', 'Cameron', 240);
+// console.log(film);
+
+const Book = function (title, author) {
+	this.title = title;
+	this.author = author;
+};
+
+Book.prototype.buy = function () {
+	console.log('Buy');
+};
+
+const AudioBook = function (title, author, lenMin) {
+	Book.call(this, title, author);
+	this.lenMin = lenMin;
+};
+
+AudioBook.prototype = Object.create(Book.prototype);
+AudioBook.prototype.constructor = AudioBook;
+
+AudioBook.prototype.log = function () {
+	console.log(`${this.title} - ${this.lenMin}`);
+};
+
+const book = new AudioBook('Test book', 'Author Andrew', 20 * 60);
+book.log();
+book.buy();
+console.log(book);

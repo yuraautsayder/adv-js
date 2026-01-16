3cd8d66fb4af86739fdd40b9b18e68c4ff891b81
Yuriy Meresin <yuriymeresin@gmail.com>
2026-01-16 21:23:45 +0300

    upd

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-'use strict';
+'user sctrict';
+
+class Film {
+	#name;
+	#author;
+	rating;
+	#length;
+
+	constructor(name, author, length) {
+		this.#name = name;
+		this.#author = author;
+		this.#length = length;
+	}
+
+	get name() {
+		return this.#name;
+	}
+
+	get author() {
+		return this.#author;
+	}
+
+	get length() {
+		return this.#length;
+	}
+}
+
+const film = new Film('Avatar', 'Cameron', 240);
+console.log(film);

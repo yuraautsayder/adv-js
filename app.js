d2cc0ac53a36b5975042b5611b051bd0d6bdb60c
Yuriy Meresin <work@fluddev.ru>
2026-01-20 23:43:50 +0300

    upd

--- app.js
+++ app.js
@@ -1,62 +1,36 @@
 'user sctrict';
-// class Book {
-// 	constructor(title, author) {
-// 		this.title = title;
-// 		this.author = author;
-// 	}
-// 	buy() {
-// 		console.log('Buy');
-// 	}
-// }
 
-// class AudioBook extends Book {
-// 	constructor(title, author, lenMin) {
-// 		super(title, author);
-// 		this.lenMin = lenMin;
-// 	}
-// 	log() {
-// 		console.log(`${this.title} - ${this.lenMin}`);
-// 	}
-// }
+class Wallet {
+	balance = 0;
 
-// const book = new AudioBook('kniga 1', 'author misha', 60 * 20);
-// book.log();
-// book.buy();
-class Enemy {
-	health;
-	constructor(health) {
-		this.health = health;
+	add(sum) {
+		this.balance += sum;
+		return this;
 	}
 
-	recieveDamage(damage) {
-		this.health = this.health - damage;
-		console.log(this.health);
+	remove(sum) {
+		this.balance -= sum;
+		return this;
 	}
 }
 
-class Sword {
-	#damage;
-	constructor(damage) {
-		this.#damage = damage;
+const wallet = new Wallet();
+const res = wallet.add(100).remove(10);
+console.log(wallet);
+
+class Builder {
+	house = {};
+	addRoof() {
+		this.house.roof = 'Roof';
+		return this;
 	}
-	strike(enemy) {
-		enemy.recieveDamage(this.#damage);
+	addFloor() {
+		this.house.floor = 'Floor';
+		return this;
+	}
+	execute() {
+		return this.house;
 	}
 }
-
-class Orc extends Enemy {
-	constructor(health) {
-		super(health);
-	}
-
-	recieveDamage(damage) {
-		if (Math.random() > 0.5) {
-			this.health = this.health - damage;
-			console.log(this.health);
-		}
-	}
-}
-
-const enemy = new Enemy(10);
-const sword = new Sword(3);
-sword.strike(enemy);
+const res2 = new Builder().addRoof().addFloor().execute();
+console.log(res2);

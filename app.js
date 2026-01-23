eaed769c300af1ad5ac4d73c0289c1c0af0a6845
Yuriy Meresin <work@fluddev.ru>
2026-01-23 22:50:18 +0300

    pbs

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 'use strict';
 
-class Treasure {
-	value = 0;
-}
-
-class Coin extends Treasure {
-	value = 1;
-}
-
-class Crystal extends Treasure {
-	value = 10;
-}
-
-class Brilliant extends Treasure {
-	value = 20;
-}
-
-class Inventory {
-	#score;
-	pick(Treasure) {
-		this.#score += treasure.value;
+class User {
+	#role = 'user';
+	getRole() {
+		return this.#role;
 	}
 }
+
+class Admin extends User {
+	#role = ['user', 'admin'];
+
+	getRole() {
+		return this.#role.join(', ');
+	}
+}
+
+function logRole(user) {
+	console.log('Role: ' + user.getRole().toUpperCase());
+}
+
+logRole(new User());
+logRole(new Admin());

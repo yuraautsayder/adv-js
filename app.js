58dce46d7c5ad8784f9815301571c482e12d46c0
Yuriy Meresin <work@fluddev.ru>
2026-01-24 22:00:18 +0300

    9-pokemon

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const request = new XMLHttpRequest();
-request.open('GET', 'https://dummyjson.com/products');
+request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
 request.send();
 
 request.addEventListener('load', function () {
-	const { products } = JSON.parse(this.responseText);
-	const sum = products.reduce((acc, p) => (acc += p.price), 0);
-	console.log(sum / products.length);
+	const { abilities } = JSON.parse(this.responseText);
+	abilities.forEach(({ ability }) => {
+		const abilityRequest = new XMLHttpRequest();
+		abilityRequest.open('GET', ability.url);
+		abilityRequest.send();
+		abilityRequest.addEventListener('load', function () {
+			const { effect_entries } = JSON.parse(this.responseText);
+			const enEffect = effect_entries.find(
+				(entry) => entry.language.name === 'en',
+			);
+			console.log(enEffect.effect);
+		});
+	});
 });

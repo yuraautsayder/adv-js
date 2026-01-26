e297a40460ce69dda41b5adbaeb39746527ed875
Yuriy Meresin <work@fluddev.ru>
2026-01-26 21:29:56 +0300

    promise chain

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 'use strict';
-
-fetch('https://dummyjson.com/products/1')
+/*
+'https://dummyjson.com/products/1'
+*/
+fetch('https://dummyjson.com/products')
+	.then((response) => response.json())
+	.then(({ products }) => {
+		console.log(products);
+		return fetch('https://dummyjson.com/products/' + products[0].id);
+	})
 	.then((response) => response.json())
 	.then((data) => {
 		console.log(data);

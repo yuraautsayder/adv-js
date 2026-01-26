0dbca8e06d2665d70e99d1495a7938282a17e300
Yuriy Meresin <work@fluddev.ru>
2026-01-26 22:15:54 +0300

    reject

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ fetch('https://dummyjson.com/products')
 	.then((response) => response.json())
 	.then(({ products }) => {
 		console.log(products);
-		return fetch('https://dummyjson.com/products/' + products[0].id);
+		return fetch('https://dddummyjson.com/products/' + products[0].id);
 	})
 	.then((response) => response.json())
 	.then((data) => {
 		console.log(data);
-	});
+	})
+	.catch((error) => console.log(error));
